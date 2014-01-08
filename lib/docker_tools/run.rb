58915e75bb711e6f2aad6b008517dfa6f3904464
janstenpickle <jansen.chris@gmail.com>
2014-01-08 19:53:46 +0000

    sort init args

--- lib/docker_tools/run.rb
+++ lib/docker_tools/run.rb
@@ -4,7 +4,7 @@ module DockerTools
   class Run
     attr_accessor :container
 
-    def initialize(name, registry, tag, volumes = [], command = nil)
+    def initialize(name, registry, tag, command = nil, volumes = [])
       @name = name
       @registry = registry
       @tag = tag
@@ -15,8 +15,9 @@ module DockerTools
 
     private
     def create_container
-      create_args = { 'Image' => "#{@registry}/#{@name}:#{@tag}", 'Tty' => true, 'Volumes' => volumes_create }
-      create_args['Cmd'] = @command unless @command.nil?
+      create_args = { 'Image' => "#{@registry}/#{@name}:#{@tag}", 'Tty' => true }
+      create_args['Cmd'] = [@command] unless @command.nil?
+      create_arfs['Volumes'] = volumes_create if @volumes.kind_of?(Array) and @volumes.size > 0
       container = Docker::Container.create(create_args)
       container.start('Binds' => @volumes)
       container
