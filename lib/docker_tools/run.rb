129383c3572724f5767941080303b3ac8577acc1
janstenpickle <jansen.chris@gmail.com>
2014-01-29 16:23:30 +0000

    Allow containers to be run by image ID

--- lib/docker_tools/run.rb
+++ lib/docker_tools/run.rb
@@ -4,22 +4,24 @@ module DockerTools
   class Run
     attr_accessor :container
 
-    def initialize(name, registry, tag, command = nil, volumes = [])
+    def initialize(name, registry, tag, image = nil, command = nil, volumes = [])
       @name = name
       @registry = registry
       @tag = tag
       @command = command
       @volumes = volumes
-      @container = create_container
+      @image = "#{@registry}/#{@name}:#{@tag}" if image = nil?
+      @image = image unless image = nil?
+      @container = create_container(@image, @command, @volumes)
     end
 
     private
-    def create_container
-      create_args = { 'Image' => "#{@registry}/#{@name}:#{@tag}", 'Tty' => false }
-      create_args['Cmd'] = @command.strip.split(/\s+/) unless @command.nil?
-      create_args['Volumes'] = volumes_create if @volumes.kind_of?(Array) and @volumes.size > 0
+    def create_container(image, command, volumes)
+      create_args = { 'Image' => image, 'Tty' => false }
+      create_args['Cmd'] = command.strip.split(/\s+/) unless command.nil?
+      create_args['Volumes'] = volumes_create if volumes.kind_of?(Array) and volumes.size > 0
       container = Docker::Container.create(create_args)
-      container.start('Binds' => @volumes)
+      container.start('Binds' => volumes)
       container
     end
     def volumes_create
