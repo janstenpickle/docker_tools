4d616b0df97e27d61bb4d44e5a718d1581ed4cae
janstenpickle <jansen.chris@gmail.com>
2014-06-26 10:59:25 +0100

    Make output parsing fail gracefully and revert docker api version

--- lib/docker_tools/util.rb
+++ lib/docker_tools/util.rb
@@ -2,14 +2,19 @@ module DockerTools::Util
   module_function
 
   def parse_output(body, &block)
-    if body.include?('}{')
-      body.split('}{').each do | line |
-        line = "{#{line}" unless line =~ /^{/
-        line = "#{line}}" unless line =~ /}$/
-        block.call(Docker::Util.parse_json(line))
+    begin
+      if body.include?('}{')
+        body.split('}{').each do | line |
+          line = "{#{line}" unless line =~ /^{/
+          line = "#{line}}" unless line =~ /}$/
+          block.call(Docker::Util.parse_json(line))
+        end
+      else
+        block.call(Docker::Util.parse_json(body))
       end
-    else
-      block.call(Docker::Util.parse_json(body))
+    rescue
+      puts 'WARN: Could not parse output from docker:'
+      puts body
     end
   end
 end
